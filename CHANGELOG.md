4fe142fd4da1801d0f5c46d59384e244732c4d21
Seth Vargo <seth@sethvargo.com>
2017-08-01 21:48:07 -0400

    Update release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 ## envconsul CHANGELOG
 
-## v0.7.0.rc1 (July 27, 2017)
+## v0.7.0 (August 1, 2017)
 
 BREAKING CHANGES:
 
--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 import "fmt"
 
-const Version = "0.7.0.rc1"
+const Version = "0.7.0"
 
 var (
 	Name      string
