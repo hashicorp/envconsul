a6f4c0ed022e3622a7913acb08ef0b6f758c5046
freddygv <vallenilla.fr@gmail.com>
2019-04-23 16:33:59 -0600

    Add test for Runner.applyConfigEnv

--- runner_test.go
+++ runner_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 	"testing"
 
@@ -103,6 +104,92 @@ func TestRunner_appendSecrets(t *testing.T) {
 			}
 			if ok && value != secretValue {
 				t.Fatalf("values didn't match, expected (%s), got (%s)", secretValue, value)
+			}
+		})
+	}
+}
+
+func TestRunner_configEnv(t *testing.T) {
+	t.Parallel()
+
+	tt := []struct {
+		name      string
+		env       map[string]string
+		pristine  bool
+		custom    []string
+		whitelist []string
+		blacklist []string
+		output    map[string]string
+	}{
+		// pristine env with no custom vars leads to empty env
+		{
+			name:     "pristine without custom vars",
+			env:      map[string]string{"PATH": "/bin"},
+			pristine: true,
+			output:   map[string]string{},
+		},
+		// pristine env with custom vars only keeps custom vars
+		{
+			name:     "pristine with custom vars",
+			env:      map[string]string{"PATH": "/bin"},
+			pristine: true,
+			custom:   []string{"GOPATH=/usr/go"},
+			output:   map[string]string{"GOPATH": "/usr/go"},
+		},
+		// custom vars overwrite input vars
+		{
+			name:   "custom and dependency env",
+			env:    map[string]string{"PATH": "/bin"},
+			custom: []string{"PATH=/usr/bin"},
+			output: map[string]string{"PATH": "/usr/bin"},
+		},
+		// whitelist filters input by key
+		{
+			name:      "whitelist only",
+			env:       map[string]string{"GOPATH": "/usr/go", "GO111MODULES": "true", "PATH": "/bin"},
+			whitelist: []string{"GO*"},
+			output:    map[string]string{"GOPATH": "/usr/go", "GO111MODULES": "true"},
+		},
+		// blacklist takes precedence over whitelist
+		{
+			name:      "whitelist and blacklist",
+			env:       map[string]string{"GOPATH": "/usr/go", "PATH": "/bin", "EDITOR": "vi"},
+			whitelist: []string{"GO*", "EDITOR"},
+			blacklist: []string{"GO*"},
+			output:    map[string]string{"EDITOR": "vi"},
+		},
+		// custom attached after white/black list rules are applied
+		{
+			name:      "blacklist and custom",
+			env:       map[string]string{"PATH": "/bin", "EDITOR": "vi"},
+			blacklist: []string{"EDITOR*"},
+			custom:    []string{"EDITOR=nvim"},
+			output:    map[string]string{"EDITOR": "nvim", "PATH": "/bin"},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			cfg := Config{
+				Exec: &config.ExecConfig{
+					Env: &config.EnvConfig{
+						Pristine:  &tc.pristine,
+						Blacklist: tc.blacklist,
+						Whitelist: tc.whitelist,
+						Custom:    tc.custom,
+					},
+				},
+			}
+			c := DefaultConfig().Merge(&cfg)
+			r, err := NewRunner(c, true)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			result := r.applyConfigEnv(tc.env)
+
+			if !reflect.DeepEqual(result, tc.output) {
+				t.Fatalf("expected: %v\n got: %v", tc.output, result)
 			}
 		})
 	}
