9a1923b0338a65c116ddf878f91525bf05fca7c8
jdotlu <jdotlu@gmail.com>
2019-06-07 11:28:06 -0400

    Add former tests as well

--- runner_test.go
+++ runner_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 	"testing"
 
@@ -197,6 +198,174 @@ func TestRunner_appendSecrets(t *testing.T) {
 			}
 			if ok && value != secretValue2 {
 				t.Fatalf("values didn't match, expected (%s), got (%s)", secretValue2, value)
+			}
+		})
+	}
+}
+
+func TestRunner_appendPrefixes(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name     string
+		path     string
+		noPrefix *bool
+		data     []*dependency.KeyPair
+		keyName  string
+	}{
+		{
+			name:     "false noprefix appends path",
+			path:     "app/my_service",
+			noPrefix: config.Bool(false),
+			data: []*dependency.KeyPair{
+				&dependency.KeyPair{
+					Key:   "mykey",
+					Value: "myValue",
+				},
+			},
+			keyName: "app_my_service_mykey",
+		},
+		{
+			name:     "true noprefix excludes path",
+			path:     "app/my_service",
+			noPrefix: config.Bool(true),
+			data: []*dependency.KeyPair{
+				&dependency.KeyPair{
+					Key:   "mykey",
+					Value: "myValue",
+				},
+			},
+			keyName: "mykey",
+		},
+		{
+			name:     "null noprefix excludes path",
+			path:     "app/my_service",
+			noPrefix: nil,
+			data: []*dependency.KeyPair{
+				&dependency.KeyPair{
+					Key:   "mykey",
+					Value: "myValue",
+				},
+			},
+			keyName: "mykey",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			cfg := Config{
+				Prefixes: &PrefixConfigs{
+					&PrefixConfig{
+						Path:     config.String(tc.path),
+						NoPrefix: tc.noPrefix,
+					},
+				},
+			}
+			c := DefaultConfig().Merge(&cfg)
+			r, err := NewRunner(c, true)
+			if err != nil {
+				t.Fatal(err)
+			}
+			kvq, err := dependency.NewKVListQuery(tc.path)
+			if err != nil {
+				t.Fatal(err)
+			}
+			env := make(map[string]string)
+			appendError := r.appendPrefixes(env, kvq, tc.data)
+			if appendError != nil {
+				t.Fatalf("got err: %s", appendError)
+			}
+
+			if len(env) > 1 {
+				t.Fatalf("Expected only 1 value in this test")
+			}
+
+			var value string
+			value, ok := env[tc.keyName]
+			if !ok {
+				t.Fatalf("expected (%s) key, but was not found", tc.keyName)
+			}
+			if ok && value != tc.data[0].Value {
+				t.Fatalf("values didn't match, expected (%s), got (%s)", tc.data[0].Value, value)
+			}
+		})
+	}
+}
+
+func TestRunner_configEnv(t *testing.T) {
+	t.Parallel()
+
+	tt := []struct {
+		name      string
+		env       map[string]string
+		pristine  bool
+		custom    []string
+		whitelist []string
+		blacklist []string
+		output    map[string]string
+	}{
+		{
+			name:     "pristine env with no custom vars yields empty env",
+			env:      map[string]string{"PATH": "/bin"},
+			pristine: true,
+			output:   map[string]string{},
+		},
+		{
+			name:     "pristine env with custom vars only keeps custom vars",
+			env:      map[string]string{"PATH": "/bin"},
+			pristine: true,
+			custom:   []string{"GOPATH=/usr/go"},
+			output:   map[string]string{"GOPATH": "/usr/go"},
+		},
+		{
+			name:   "custom vars overwrite input vars",
+			env:    map[string]string{"PATH": "/bin"},
+			custom: []string{"PATH=/usr/bin"},
+			output: map[string]string{"PATH": "/usr/bin"},
+		},
+		{
+			name:      "whitelist filters input by key",
+			env:       map[string]string{"GOPATH": "/usr/go", "GO111MODULES": "true", "PATH": "/bin"},
+			whitelist: []string{"GO*"},
+			output:    map[string]string{"GOPATH": "/usr/go", "GO111MODULES": "true"},
+		},
+		{
+			name:      "blacklist takes precedence over whitelist",
+			env:       map[string]string{"GOPATH": "/usr/go", "PATH": "/bin", "EDITOR": "vi"},
+			whitelist: []string{"GO*", "EDITOR"},
+			blacklist: []string{"GO*"},
+			output:    map[string]string{"EDITOR": "vi"},
+		},
+		{
+			name:      "custom takes precedence over blacklist",
+			env:       map[string]string{"PATH": "/bin", "EDITOR": "vi"},
+			blacklist: []string{"EDITOR*"},
+			custom:    []string{"EDITOR=nvim"},
+			output:    map[string]string{"EDITOR": "nvim", "PATH": "/bin"},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			cfg := Config{
+				Exec: &config.ExecConfig{
+					Env: &config.EnvConfig{
+						Pristine:  &tc.pristine,
+						Blacklist: tc.blacklist,
+						Whitelist: tc.whitelist,
+						Custom:    tc.custom,
+					},
+				},
+			}
+			c := DefaultConfig().Merge(&cfg)
+			r, err := NewRunner(c, true)
+			if err != nil {
+				t.Fatal(err)
+			}
+			result := r.applyConfigEnv(tc.env)
+
+			if !reflect.DeepEqual(result, tc.output) {
+				t.Fatalf("expected: %v\n got: %v", tc.output, result)
 			}
 		})
 	}
